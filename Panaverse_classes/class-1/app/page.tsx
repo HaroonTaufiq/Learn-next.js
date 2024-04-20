6fc933ff969dba5f10f88063e0f53fb4871e427f
Haroon <itx.haroon1@gmail.com>
2024-04-20 14:15:23 +0500

    class end

--- Panaverse_classes/class-1/app/page.tsx
+++ Panaverse_classes/class-1/app/page.tsx
@@ -2,21 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className= "grid grid-cols-4 grid-flow-row">
-      <div className="bg-flex-300 p-2">block</div>
-      <div className="bg-flex-300 p-2">block</div>
-      <div className="bg-flex-300 p-2">block</div>
-      <div className="bg-flex-300 p-2">block</div>
-      <div className="bg-flex-300 p-2">block</div>
-      <div className="bg-flex-300 p-2">block</div>
-      <div className="bg-flex-300 p-2">block</div>
-      <div className="bg-flex-300 p-2">block</div>
-      <div className="bg-flex-300 p-2">block</div>
-      <div className="bg-flex-300 p-2">block</div>
-      <div className="bg-flex-300 p-2">block</div>
-      <div className="bg-flex-300 p-2">block</div>
-      <div className="bg-flex-300 p-2">block</div>
+    <div className="grid grid-cols-4 grid-flow-row">
+      <div className="flex bg-red-300 p-2">block</div>
+      <div className="flex bg-red-300 p-2">block</div>
+      <div className="flex bg-red-300 p-2">block</div>
+      <div className="flex bg-red-300 p-2">block</div>
+      <div className="flex bg-red-300 p-2">block</div>
+      <div className="flex bg-red-300 p-2">block</div>
+      <div className="flex bg-red-300 p-2">block</div>
+      <div className="flex bg-red-300 p-2">block</div>
+      <div className="flex bg-red-300 p-2">block</div>
+      <div className="flex bg-red-300 p-2">block</div>
+      <div className="flex bg-red-300 p-2">block</div>
+      <div className="flex bg-red-300 p-2">block</div>
     </div>
-
   );
 }
